d64f6e96b481047404dc3678830ee8342f3e8713
abduljwad <jawad.sayah875@gmail.com>
2022-11-20 01:00:11 +0300

    add new file

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Front-End", "Developer", "Web Designer", "UI/UX Designer", "Freelancer"],
+        strings: ["Front-End", "Developer", "Web Design", "UI/UX Design", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Front-End", "Developer", "Web Designer", "UI/UX Designer", "Freelancer"],
+        strings: ["Front-End", "Developer", "Web Design", "UI/UX Design", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
